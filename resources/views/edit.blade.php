d3b7ef85dc4a271c4f1dc572ae108962c28970e2
wolvecode <wolvecode@gmail.com>
2021-05-04 20:42:20 +0100

    changes

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -3,25 +3,27 @@
 @section('title', 'Edit Record')
 
 @section('content')
-    <div class="col-12 col-md-4">
+    <div class="col-12 col-md-6">
         <div class="row">
-            <h4>Hello {{$user->name}}</h4>
             <div class="card-body">
                 <div class="row">
-                    <div class="col-12 col-md-4">
+                    <div class="col-12 col-md-6">
                         <form method="post" action="/user/{{$user->id}}">
                             @method('patch')
                             @csrf
                             <div class="form-group">
+                                <h4>Hello {{$user->name}}</h4>
+                            </div>
+                            <div class="form-group">
                                 <label for="exampleInputEmail1">Fullname</label>
-                                <input type="text" class="form-control" name="name" id="exampleInputEmail1" value="{{ old('name') }}" aria-describedby="emailHelp">
+                                <input type="text" class="form-control" value="{{$user->name}}" name="name" id="exampleInputEmail1" value="{{ old('name') }}" aria-describedby="emailHelp">
                                 @error('name')
                                 <small class="form-text text-danger">{{ $message }}</small>
                                 @enderror
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputPassword1">Age</label>
-                                <input type="text" name="age" class="form-control" id="exampleInputPassword1" value="{{ old('age') }}">
+                                <input type="text" name="age" value="{{$user->age}}" class="form-control" id="exampleInputPassword1" value="{{ old('age') }}">
                                 @error('age')
                                 <small class="form-text text-danger">{{ $message }}</small>
                                 @enderror
